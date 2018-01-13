f620b8e6fe46b61d0fa64055ebd717c8e2163b99
Johannah Sims <jjrsims@gmail.com>
2018-01-13 18:33:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,5 @@
-def fizzbuzz(fizzbuzz_3)
+def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-end
-
-def fizzbuzz(fizzbuzz_5)
-  if int % 5 == 0
-    "Buzz"
-  end
-end
-
-def fizzbuzz(fizzbuzz_" ")
-  elsif int % 3 && 5
-    "FizzBuzz"
   end
 end
